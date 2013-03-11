85456176edc55c7b6aa78c843bbb41ec9f6d66d0
Andreas Tarandi <torandi@gmail.com>
2013-03-11 14:24:40 +0100

    Add from_id test

--- tests/suites/BasicTest.php
+++ tests/suites/BasicTest.php
@@ -4,6 +4,21 @@ require_once dirname(__FILE__) . "/../includes.php";
 
 class BasicTest extends DatabaseTestCase {
 
+	public function testFromId() {
+		global $db;
+		if(!$db->query("INSERT INTO `model1` SET `int1` = 5, `str1` = 'foobar'")) {
+			throw new Exception("Failed to insert model by manual query: ".$db->error);
+		}
+		$id = $db->insert_id;
+		$obj = Model1::from_id($id);
+		$this->assertNotNull($obj);
+		$this->assertEquals($obj->int1, 5);
+		$this->assertEquals($obj->str1, "foobar");
+	}
+
+	/**
+	 * @depends testFromId
+	 */
 	public function testInsert() {
 		$model1 = new Model1();
 		$model1->int1 = 1;
@@ -20,5 +35,24 @@ class BasicTest extends DatabaseTestCase {
 		$this->assertEquals($model1->int1, 1);
 		$this->assertEquals($model1->str1, "Test");
 	}
+
+	/**
+	 * @depends testFromId
+	 */
+	public function testArrayInsert() {
+		$model1 = new Model1(array('int1'=>1, 'str1' => "Test"));
+		$model1->commit();
+
+		$this->assertNotNull($model1->id);
+
+		$id = $model1->id;
+		unset($model1);
+
+		$model1 = Model1::from_id($id);
+
+		$this->assertEquals($model1->int1, 1);
+		$this->assertEquals($model1->str1, "Test");
+	}
+
 }
 
