91a3dd1da6d329a1eb8b44360a1a2f9f60dbf30e
Andreas Tarandi <torandi@gmail.com>
2013-03-11 19:26:27 +0100

    Add test for commit invalidating cache

--- tests/suites/BasicObject/CacheTest.php
+++ tests/suites/BasicObject/CacheTest.php
@@ -17,6 +17,15 @@ class CacheTest extends DatabaseTestCase {
 		}
 	}
 
-	public function testFromField() {
+	public function testEditAndLoad() {
+		$m1 = Blueprint::make('Model1');
+		$m1 = Model1::from_id($m1->id);
+		$val = $m1->int1 + 10;
+		$m1->int1 = $val;
+		$m1->commit();
+
+		$m1 = Model1::from_id($m1->id);
+		$this->assertEquals($val, $m1->int1);
+
 	}
 }
