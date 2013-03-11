5de8120a5c56f586276d581a6548588d7a8e69fb
Andreas Tarandi <torandi@gmail.com>
2013-03-11 16:49:27 +0100

    Add Blueprint-class for creating model instances

---
+++ tests/README.md
@@ -0,0 +1,15 @@
+Tests
+=====
+
+To run the tests:
+
+	./tests.sh
+
+To create new tests, add files to suites/ (see exists tests for examples)
+
+Blueprints
+=======
+
+To ease creation of tests there are a Blueprint-class for creating models.
+Documentation on that is located in the blueprints-directory.
+I recommend using Blueprints for your own projects BO-related tests too.
--- tests/includes.php
+++ tests/includes.php
@@ -7,6 +7,7 @@ include realpath(dirname(__FILE__)) . "/database.php";
 include realpath(dirname(__FILE__)) . "/MC.php";
 include realpath(dirname(__FILE__)) . "/../BasicObject.php";
 include realpath(dirname(__FILE__)) . "/../ValidatingBasicObject.php";
+include realpath(dirname(__FILE__)) . "/Blueprint.php";
 include realpath(dirname(__FILE__)) . "/DatabaseTestCase.php";
 
 foreach(glob(realpath(dirname(__FILE__)) . "/models/*.php") as $filename) {
